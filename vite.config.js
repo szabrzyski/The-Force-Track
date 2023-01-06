76b60ca4ba6f64185768440a1339ca20a61703fc
Sebastian Zabrzyski <sebastianzabrzyski@gmail.com>
2023-01-06 14:48:11 +0100

    add vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
@@ -7,5 +8,20 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
+    build: {
+        chunkSizeWarningLimit: 2000,
+    },
+    define: {
+        __VUE_PROD_DEVTOOLS__: false,
+        __VUE_OPTIONS_API__: false,
+    }
 });
