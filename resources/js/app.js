eba2fad4a685c4be22a09353519f57dccff28c84
Sebastian Zabrzyski <sebastianzabrzyski@gmail.com>
2023-01-06 15:01:05 +0100

    update vite config and add test page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-import './bootstrap';
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from "./components/layouts/App.vue";
+import * as bootstrap from 'bootstrap';
+import axios from 'axios';
+
+window.bootstrap = bootstrap;
+window.axios = axios;
+
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(pinia).mount("#app");
