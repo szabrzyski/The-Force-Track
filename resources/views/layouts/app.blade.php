c556a7c097d8bc33bb1fbef8a28964b0eff18033
Sebastian Zabrzyski <sebastianzabrzyski@gmail.com>
2023-01-06 15:24:09 +0100

    add login page route

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title', 'TheForceTrack')</title>
+    @stack('head')
+    @vite(['resources/js/app.js'])
+    @stack('js')
+</head>
+
+<body>
+    <main id="app">
+    </main>
+</body>
+
+</html>
