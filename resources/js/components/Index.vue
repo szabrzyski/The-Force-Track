246dfeb54555223c5e0f6042ea81b3b903e44e76
Sebastian Zabrzyski <sebastianzabrzyski@gmail.com>
2023-01-08 13:52:12 +0100

    add alert to index page

--- resources/js/components/Index.vue
+++ resources/js/components/Index.vue
@@ -1,5 +1,6 @@
 <script setup>
 
+import Alert from './partials/Alert.vue';
 const emit = defineEmits(['viewLoaded']);
 
 emit('viewLoaded');
@@ -8,8 +9,9 @@ emit('viewLoaded');
 
 <template>
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-12">
+        <div class="row justify-content-center mt-2 mt-sm-3">
+            <Alert />
+            <div class="col-12 mt-lg-3">
                 Index page
             </div>
         </div>
