58b1f790c4e5600e22e234ca3340d66390d45a17
Sebastian Zabrzyski <sebastianzabrzyski@gmail.com>
2023-01-08 14:03:21 +0100

    add categories and issues migrations

---
+++ database/migrations/2023_01_08_125237_create_issues_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('issues', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('subject');
+            $table->text('description');
+            $table->enum('status', ['Open', 'In progress', 'Closed']);
+            $table->timestamps();
+            $table->foreign('category_id')->references('id')->on('categories')->cascadeOnUpdate()
+                ->restrictOnDelete();
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnUpdate()
+                ->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('issues');
+    }
+};
